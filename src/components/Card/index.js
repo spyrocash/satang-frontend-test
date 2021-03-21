d96dd4d092153960aee7a46f4f229e7853ffd6c5
spyrocash <spyrocash@gmail.com>
2021-03-22 00:37:11 +0700

    reponsive

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,34 +5,69 @@ import './style.scss'
 
 function Card({ logo, name, fullname, price, changePercent24h, volumn24h }) {
   const isPriceUp = changePercent24h > 0
+  const renderDesktop = () => {
+    return (
+      <div className="desktop">
+        <div className="row-1">
+          <div className="head-info">
+            <div className="logo">
+              <img src={logo} alt="" />
+            </div>
+            <div className="name-block">
+              <p className="name">{name}</p>
+              <p className="fullname">{fullname}</p>
+            </div>
+          </div>
+        </div>
+        <div className="row-2">
+          <p className="price">{numeral(price || 0).format('$0,0.00')}</p>
+        </div>
+        <div className="row-3">
+          <div className="change-24h">
+            <p className={`value price-${isPriceUp ? 'up' : 'down'}`}>
+              {numeral(changePercent24h || 0).format('+0,0.00')}% {isPriceUp ? <ArrowUp /> : <ArrowDown />}
+            </p>
+            <p className="label">24h Change</p>
+          </div>
+          <div className="volumn-24h">
+            <p className="value">{numeral(volumn24h || 0).format('0,0.00')}</p>
+            <p className="label">24h Volume</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  const renderMobile = () => {
+    return (
+      <div className="mobile">
+        <div className="head-info">
+          <img className="logo" src={logo} alt="" />
+          <p className="name">{name}</p>
+          <p className="fullname">{fullname}</p>
+        </div>
+        <ul className="detail-list">
+          <li>
+            <p className="label">Prices</p>
+            <p className="value price">{numeral(price || 0).format('$0,0.00')}</p>
+          </li>
+          <li>
+            <p className="label">24h Volume</p>
+            <p className="value volume">{numeral(volumn24h || 0).format('0,0.00')}</p>
+          </li>
+          <li>
+            <p className="label">24h change</p>
+            <p className={`value price-${isPriceUp ? 'up' : 'down'}`}>
+              {numeral(changePercent24h || 0).format('+0,0.00')}% {isPriceUp ? <ArrowUp /> : <ArrowDown />}
+            </p>
+          </li>
+        </ul>
+      </div>
+    )
+  }
   return (
     <div className="card-component">
-      <div className="row-1">
-        <div className="head-info">
-          <div className="logo">
-            <img src={logo} alt="" />
-          </div>
-          <div className="name-block">
-            <p className="name">{name}</p>
-            <p className="fullname">{fullname}</p>
-          </div>
-        </div>
-      </div>
-      <div className="row-2">
-        <p className="price">{numeral(price || 0).format('$0,0.00')}</p>
-      </div>
-      <div className="row-3">
-        <div className="change-24h">
-          <p className={`value price-${isPriceUp ? 'up' : 'down'}`}>
-            {numeral(changePercent24h || 0).format('+0,0.00')}% {isPriceUp ? <ArrowUp /> : <ArrowDown />}
-          </p>
-          <p className="label">24h change</p>
-        </div>
-        <div className="volumn-24h">
-          <p className="value">{numeral(volumn24h || 0).format('0,0.00')}</p>
-          <p className="label">24h Volume</p>
-        </div>
-      </div>
+      <div className="desktop-display">{renderDesktop()}</div>
+      <div className="mobile-display">{renderMobile()}</div>
     </div>
   )
 }
--- src/store/cryptocurrency.js
+++ src/store/cryptocurrency.js
@@ -2,15 +2,181 @@ import { action, thunk } from 'easy-peasy'
 import _ from 'lodash'
 import { apiGetCryptoCurrency, socketGetCryptoCurrencyUrl } from 'services'
 
-export const mapFieldCryptoCurrency = item => {
-  const { symbol, lastPrice, openPrice, quoteVolume } = item
+const staticItems = [
+  {
+    name: 'BTC',
+    fullname: 'Bitcoin',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/btc.png',
+  },
+  {
+    name: 'USDT',
+    fullname: 'TetherUS',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/usdt.png',
+  },
+  {
+    name: 'ADA',
+    fullname: 'Cardano',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/ada.png',
+  },
+  {
+    name: 'ETH',
+    fullname: 'Ethereum',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/eth.png',
+  },
+  {
+    name: 'BNB',
+    fullname: 'BNB',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/bnb.png',
+  },
+  {
+    name: 'DOT',
+    fullname: 'Polkadot',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/dot.png',
+  },
+  {
+    name: 'XRP',
+    fullname: 'Ripple',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/xrp.png',
+  },
+  {
+    name: 'TRX',
+    fullname: 'TRON',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/trx.png',
+  },
+  {
+    name: 'HBAR',
+    fullname: 'Hedera Hashgraph',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/hbar.png',
+  },
+  {
+    name: 'BAT',
+    fullname: 'Basic Attention Token',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/bat.png',
+  },
+  {
+    name: 'DOGE',
+    fullname: 'Dogecoin',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coin/38634057-e551-44e0-8ff5-033d4ee0b612.png',
+  },
+  {
+    name: 'XZC',
+    fullname: 'Firo',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/xfr.png',
+  },
+  {
+    name: 'XLM',
+    fullname: 'Stellar Lumens',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/xlm.png',
+  },
+  {
+    name: 'LTC',
+    fullname: 'Litecoin',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/ltc.png',
+  },
+  {
+    name: 'BAND',
+    fullname: 'BAND',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/band.png',
+  },
+  {
+    name: 'ATOM',
+    fullname: 'Cosmos',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/atom.png',
+  },
+  {
+    name: 'LINK',
+    fullname: 'ChainLink',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/link.png',
+  },
+  {
+    name: 'ALGO',
+    fullname: 'Algorand',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/algo.png',
+  },
+  {
+    name: 'JFIN',
+    fullname: 'JFIN Coin',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coin/7ec09a84-6de9-4649-909b-1a41f6e1b470.png',
+  },
+  {
+    name: 'EOS',
+    fullname: 'EOS',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/eos.png',
+  },
+  {
+    name: 'XTZ',
+    fullname: 'Tezos',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coin/f80559c6-3242-4c48-ab11-3e5326bf6800.png',
+  },
+  {
+    name: 'DASH',
+    fullname: 'Dash',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coin/b6c8c554-ae01-4460-866d-fafa6f19db5c.png',
+  },
+  {
+    name: 'BUSD',
+    fullname: 'BUSD',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/busd.png',
+  },
+  {
+    name: 'ZEC',
+    fullname: 'Zcash',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/zec.png',
+  },
+  {
+    name: 'ETC',
+    fullname: 'Ethereum Classic',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/etc.png',
+  },
+  {
+    name: 'USDC',
+    fullname: 'USD Coin',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/usdc.png',
+  },
+  {
+    name: 'BCH',
+    fullname: 'Bitcoin Cash',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/bch.png',
+  },
+  {
+    name: 'XMR',
+    fullname: 'Monero',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/xmr.png',
+  },
+  {
+    name: 'TUSD',
+    fullname: 'TrueUSD',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/tusd.png',
+  },
+  {
+    name: 'DAI',
+    fullname: 'Dai',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/dai.png',
+  },
+  {
+    name: 'PAXG',
+    fullname: 'PAX Gold',
+    logo: 'https://storage.googleapis.com/satang-pro/public/assets/icons/coins/paxg.png',
+  },
+]
+
+const calculateChangePercent24h = (openPrice, lastPrice) => {
   const openPriceNumber = Number(openPrice)
   const lastPriceNumber = Number(lastPrice)
-  const changePercent24h = openPriceNumber ? ((lastPriceNumber - openPriceNumber) / openPriceNumber) * 100 : 100
+  return openPriceNumber ? ((lastPriceNumber - openPriceNumber) / openPriceNumber) * 100 : 100
+}
+
+const findStaticItem = symbol => {
+  const name = _.toUpper(_.replace(symbol, '_thb', ''))
+  const staticItem = _.find(staticItems, { name })
+  return staticItem
+}
+
+export const mapFieldCryptoCurrency = item => {
+  const { symbol, openPrice, lastPrice, quoteVolume } = item
+  const changePercent24h = calculateChangePercent24h(openPrice, lastPrice)
   return {
-    logo: null,
-    name: symbol,
-    fullname: symbol,
+    ...findStaticItem(symbol),
     price: lastPrice,
     changePercent24h,
     volumn24h: quoteVolume,
@@ -18,14 +184,10 @@ export const mapFieldCryptoCurrency = item => {
 }
 
 export const mapFieldCryptoCurrencyFromSocket = item => {
-  const { c, o, q, s } = item
-  const openPriceNumber = Number(o)
-  const lastPriceNumber = Number(c)
-  const changePercent24h = openPriceNumber ? ((lastPriceNumber - openPriceNumber) / openPriceNumber) * 100 : 100
+  const { s, o, c, q } = item
+  const changePercent24h = calculateChangePercent24h(o, c)
   return {
-    logo: null,
-    name: s,
-    fullname: s,
+    ...findStaticItem(s),
     price: c,
     changePercent24h,
     volumn24h: q,
